7d6eea6945ffa84e8cf6624cb7dcce071386e923
IvanLossev <Ivan.lossev06@gmail.com>
2024-11-05 23:07:43 +0300

    Add files via upload

---
+++ after_refactoring_ilosev.cpp
@@ -0,0 +1,30 @@
+﻿#include <cmath>   
+#include <iostream>
+
+
+int main()
+{
+    const int size = 20;
+    int massive[size];
+
+
+    for (int i = 0; i < size; i++)
+    {
+        massive[i] = rand() % size;
+        std::cout << massive[i] << " ";
+    }
+
+    std::cout << "\n";
+
+    for (int i = 0; i < size / 2; i++)
+    {
+        std::swap(massive[i], massive[size - 1 - i]);
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << massive[i] << " ";
+    }
+
+    return 0;
+}
